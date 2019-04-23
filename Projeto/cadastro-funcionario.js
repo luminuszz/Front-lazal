b67f139dfeda362d3e9f225002d03d10d11454c7
davi ribeiro <luminuszz43@gmail.com>
2019-04-23 12:22:50 -0300

    Validação de funcionário

---
+++ Projeto/cadastro-funcionario.js
@@ -0,0 +1,10 @@
+function gatbut(){
+    alert("Email enviado com seucesso");
+  }
+
+  function gatbut1(){
+    var email1 = document.getElementById('email');
+
+    alert("Deseja enviar o formulario para " + email1.value +  " ?");
+
+  }
